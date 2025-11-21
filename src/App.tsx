d70a32f3da3f31a36925559a85f0401ab5ede6e6
[dyad] <git@dyad.sh>
2025-11-21 11:50:33 +1100

    [dyad] Direct chat start page like ChatGPT with pulsing background - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
+"use client";
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import ChatWindow from "./components/ChatWindow";
+import AnimatedBackground from "./components/AnimatedBackground";
 import NotFound from "./pages/NotFound";
-import GrokHeader from "./components/GrokHeader";
-import GrokHero from "./components/GrokHero";
-import GrokFeatures from "./components/GrokFeatures";
-import GrokChatWindow from "./components/GrokChatWindow";
 
 const queryClient = new QueryClient();
 
@@ -18,23 +17,14 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <div className="min-h-screen bg-black">
-          <GrokHeader />
-          <Routes>
-            <Route path="/" element={
-              <>
-                <GrokHero />
-                <GrokFeatures />
-                <div className="py-8">
-                  <div className="max-w-7xl mx-auto px-4">
-                    <GrokChatWindow />
-                  </div>
-                </div>
-              </>
-            } />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+        <div className="h-screen overflow-hidden bg-gradient-to-br from-slate-900 via-purple-900/20 to-slate-900">
+          <AnimatedBackground />
+          <div className="relative z-40 h-screen flex flex-col">
+            <Routes>
+              <Route path="/" element={<ChatWindow />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </TooltipProvider>
